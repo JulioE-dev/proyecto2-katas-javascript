f6a2e6d3dd7061eeed68df16723a3b28107f7e1c
JulioE-dev <smithcifra80@gmail.com>
2026-02-23 13:48:19 +0100

    Delete ejercicios-basicos-avanzados directory

--- ejercicios-basicos-avanzados/ejercicio41.js
+++
@@ -1,8 +0,0 @@
-function rollDice(faces) {
-    const result = Math.floor(Math.random() * faces) + 1;
-    return result;
-}
-
-console.log("Tirada con un dado tradicional de 6 caras:", rollDice(6));
-console.log("Tirada con un dado de rol  de 12 caras:", rollDice(12));
-console.log("Tirada con un dado de rol de 20 caras:", rollDice(20));
--- ejercicios-basicos-avanzados/ejercicio9.js
+++
@@ -1,9 +0,0 @@
-const numbers = [1,2,3,5,45,37,58];
-function sumNumbers(numberlist) {
-    let suma = 0;
-    for (let i = 0; i < numberlist.length; i++) {
-        suma = suma + numberlist[i];
-        }
-    return suma;
-}
-console.log("La suma total es:", sumNumbers(numbers));
